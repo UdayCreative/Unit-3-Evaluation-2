94c758e357c4f1ccb58a35364e59d38c3f1b1473
Uday Patil <104245464+UdayCreative@users.noreply.github.com>
2022-05-30 17:22:53 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,4 @@
 // Add the coffee to local storage with key "coffee"
-// async function getData(){
-//     let url = "https://masai-mock-api.herokuapp.com/coffee/menu"
-//     let res = await fetch(url)
-//     let cartCoffee = await res.json()
-//     console.log(cartCoffee.menu.data)
-// }
-//  getData
-
-//  let data = JSON.parse(localStorage.getItem("coffee")) ||
- 
-//  function append(data){
-
-//     data.forEach(function(elem,index){
-//         let div = document.createElement("div")
-//         div.setAttribute("class","wrapper")
-
-//         let img = document.createElement('img')
-//         img.src=elem.image
-
-//         let div1 = document.createElement("div")
-//         div1.setAttribute("class","infoDiv")
-
-//         let h2 = document.createElement("h2")
-//         h2.innerText = elem.title
-
-//         let p =document.createElement("p")
-//         p.innerText="Price: ${elem.price}"
-
-//         let button = document.createElement("button")
-//         button.setAttribute("id","add_to_bucket")
-        
-//         button.addEventListener("click",function(){
-//             addToBucket(elem,index)
-//         })
-//         div1.append(h2,p,button)
-//         div.append(img,div1)
-
-//         document.querySelector("menu").append("div")
-//     })
-//  }
-//  function addToBucket(elem,index){
-
-//  }
-
 const url ="https://masai-mock-api.herokuapp.com/coffee/menu"
 fetch(url).then(function(res){
     return res.json()
